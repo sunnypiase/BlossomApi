b820d35d9ba70b55a14e544564e19e2be44d97c9
Byevich Dmytro <71142218+sunnypiase@users.noreply.github.com>
2024-09-27 18:06:02 +0300

    fix images

--- Dtos/Orders/ProductInOrderDetailDto.cs
+++ Dtos/Orders/ProductInOrderDetailDto.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public int Amount { get; set; }
         public decimal ProductByAmountPrice { get; set; } // Unit Price * Amount
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public CategoryResponseDto MainCategory { get; set; }
     }
 }
